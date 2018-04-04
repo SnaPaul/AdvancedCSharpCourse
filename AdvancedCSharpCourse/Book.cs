3a65c9f44c18df02de454867d6fe51ae5f1dbe9f
SnaPaul <snajder.paul@gmail.com>
2018-04-04 21:55:41 +0200

    added the first version of the source code

---
+++ AdvancedCSharpCourse/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCSharpCourse
+{
+    public class Book : Product
+    {
+        //public string Title { get; set; }
+        public string Isbn { get; set; }
+
+        //Book(string isbn, string title)
+        //{
+        //    ISBN = isbn;
+        //    Title = title;
+        //}
+    }
+}
---
+++ AdvancedCSharpCourse/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedCSharpCourse
+{
+    //where T : IComparible
+    //whre T : Product (is a class)
+    //where T : stuct (value type)
+    //where T : class (reference type)
+    //where T : new() (has a default constructor)
+
+    class Utilities<T> where T : IComparable, new()
+    {
+        public int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        public T Max(T a, T b)
+        {
+            return a.CompareTo(b) > 0 ? a : b;
+        }
+
+        public void DoSomething(T value)
+        {
+            var obj = new T();
+        }
+    }
+}
